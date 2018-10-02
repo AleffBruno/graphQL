158a739af4276939a02b51eee9c45ebb8e69a90a
Aleff <aleffbruno@gmail.com>
2018-10-02 16:47:00 -0300

    initializing bind data to front-end

--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+//allow cross-origin rquest
+app.use(cors());
 
 //aqui eu poderia usar um async/await pra nao precizar 'adivinhar qd a conexao estara OK'
 mongoose.connect('mongodb://hue102030:hue102030@ds259711.mlab.com:59711/ndstr');
@@ -17,6 +21,6 @@ app.use('/graphql',graphqlHTTP({
     graphiql: true
 }));
 
-app.listen(3000,()=>{
-    console.log('l on 3k');
+app.listen(4000,()=>{
+    console.log('l on 4k');
 })
